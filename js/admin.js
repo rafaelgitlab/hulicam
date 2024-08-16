e47145ec03205779350a0c302949c9da08545bb1
rafaelgitlab <51089728+rafaelgitlab@users.noreply.github.com>
2024-08-16 23:22:01 +0800

    Add files via upload

---
+++ js/admin.js
@@ -0,0 +1,90 @@
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "fixed";
+    }
+}
+
+
+$(function() {
+  $("[name='username']").focus();
+});
+
+function myLogout() {
+    document.getElementById("logout");
+    alert('Succesfully Logout!');
+    window.location.href='admin.php';
+}
+
+
+$(document).ready(function(){
+        $(".acategories,.bcategories,.ccategories,.dcategories,.ecategories").hide();
+        $(".aaeye").hide();
+        $(".bbeye").hide();
+        $(".cceye").hide();
+        $(".ddeye").hide();
+        $(".eeeye").hide();
+           
+
+    $(".aeye").click(function(){
+        $(".acategories").slideToggle();
+        $(".aaeye").show();
+        $(".aeye").hide();
+    });
+
+      $(".aaeye").click(function(){
+        $(".acategories").slideToggle();
+        $(".aeye").show();
+        $(".aaeye").hide();
+    });
+
+     
+    $(".beye").click(function(){
+        $(".bcategories").slideToggle();
+        $(".bbeye").show();
+        $(".beye").hide();
+    });
+
+      $(".bbeye").click(function(){
+        $(".bcategories").slideToggle();
+        $(".beye").show();
+        $(".bbeye").hide();
+    });
+
+    $(".ceye").click(function(){
+        $(".ccategories").slideToggle();
+        $(".cceye").show();
+        $(".ceye").hide();
+    });
+
+      $(".cceye").click(function(){
+        $(".ccategories").slideToggle();
+        $(".ceye").show();
+        $(".cceye").hide();
+    });
+
+     $(".deye").click(function(){
+        $(".dcategories").slideToggle();
+        $(".ddeye").show();
+        $(".deye").hide();
+    });
+
+      $(".ddeye").click(function(){
+        $(".dcategories").slideToggle();
+        $(".deye").show();
+        $(".ddeye").hide();
+    });
+
+     $(".eeye").click(function(){
+        $(".ecategories").slideToggle();
+        $(".eeeye").show();
+        $(".eeye").hide();
+    });
+
+      $(".eeeye").click(function(){
+        $(".ecategories").slideToggle();
+        $(".eeye").show();
+        $(".eeeye").hide();
+    });
+});
